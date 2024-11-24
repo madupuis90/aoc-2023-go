98084e4c059ca911b7eb0d2b2e7f555a19c069d5
mad <madupuis90@gmail.com>
2024-11-23 22:14:06 -0500

    optimised day5 part 2 from 3 mins to 0.0001s

--- day5/main.go
+++ day5/main.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	start := time.Now()
-	// part1 := part1("input.txt")
+	part1 := part1("input.txt")
 	part2 := part2("input.txt")
 
 	fmt.Printf("Part 1: %v\n", part1)
@@ -47,14 +47,14 @@ func part1(f string) int {
 			continue
 		}
 
-		// process numbers
+		// create the filters from numbers
 		currentMap := mapFilters[len(mapFilters)-1]
 		currentMap.filters = append(currentMap.filters, createFilter(line))
 	}
 	minValue := math.MaxInt
 	var value int
 	for _, s := range seeds {
-		value = mapFilters[0].compute(s)
+		value = mapFilters[0].computeSeed(s)
 		if value < minValue {
 			minValue = value
 		}
@@ -63,7 +63,119 @@ func part1(f string) int {
 	return minValue
 }
 
-func (mf *MapFilter) compute(seed int) int {
+// pretty happy with the results, this runs in 0.0001 sec on my computer
+func part2(f string) int {
+	scanner := util.CreateScannerFromFile(f)
+	var seedRanges []*SeedRange
+	var mapFilters []*MapFilter
+
+	if scanner.Scan() {
+		seeds := parseSeeds(scanner.Text())
+		for i := 0; i < len(seeds)-1; i += 2 {
+			seedRanges = append(seedRanges, &SeedRange{lowerBound: seeds[i], upperBound: seeds[i] + seeds[i+1] - 1})
+		}
+	}
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line == "" {
+			continue
+		}
+
+		// create a new map
+		if strings.Contains(line, "map") {
+			newMapFilter := createMapFilter()
+			if len(mapFilters) > 0 {
+				mapFilters[len(mapFilters)-1].nextMap = newMapFilter
+			}
+			mapFilters = append(mapFilters, newMapFilter)
+			continue
+		}
+
+		// create the filters from numbers
+		currentMap := mapFilters[len(mapFilters)-1]
+		currentMap.filters = append(currentMap.filters, createFilter(line))
+
+	}
+
+	minValue := math.MaxInt
+	// Pass seed ranges to filters
+	for _, s := range seedRanges {
+		value := processSeedRange(s, 0, mapFilters)
+		if value < minValue {
+			minValue = value
+		}
+	}
+
+	return minValue
+}
+
+// process a seed range and return lowest value
+func processSeedRange(s *SeedRange, index int, mapFilters []*MapFilter) int {
+	minValue := math.MaxInt
+	// check if we processed all map filters, time to figure out the minimum
+	if index == len(mapFilters) {
+		// since we now have a bunch of ranges, the lower bound of the lowest range is our minimum
+		minValue = s.lowerBound
+		return minValue
+	}
+	// range intersections
+	for i, f := range mapFilters[index].filters {
+		// inside range, map the range to the next index
+		if f.upperBound >= s.upperBound && s.lowerBound >= f.lowerBound {
+			processedSeedRange := &SeedRange{lowerBound: s.lowerBound + f.boundDiff, upperBound: s.upperBound + f.boundDiff}
+			value := processSeedRange(processedSeedRange, index+1, mapFilters)
+			if value < minValue {
+				minValue = value
+			}
+			break
+		} else if s.lowerBound > f.lowerBound && s.lowerBound < f.upperBound && s.upperBound > f.upperBound {
+			// partial above range, split range and restart
+			newSeedRange1 := &SeedRange{lowerBound: s.lowerBound, upperBound: f.upperBound}
+			newSeedRange2 := &SeedRange{lowerBound: f.upperBound + 1, upperBound: s.upperBound}
+			value := processSeedRange(newSeedRange1, index, mapFilters)
+			if value < minValue {
+				minValue = value
+			}
+			value = processSeedRange(newSeedRange2, index, mapFilters)
+			if value < minValue {
+				minValue = value
+			}
+			break
+
+		} else if s.lowerBound < f.lowerBound && s.upperBound > f.lowerBound && s.upperBound < f.upperBound {
+			// partial below range, split range and restart
+			newSeedRange1 := &SeedRange{lowerBound: s.lowerBound, upperBound: f.lowerBound - 1}
+			newSeedRange2 := &SeedRange{lowerBound: f.lowerBound, upperBound: s.upperBound}
+			value := processSeedRange(newSeedRange1, index, mapFilters)
+			if value < minValue {
+				minValue = value
+			}
+			value = processSeedRange(newSeedRange2, index, mapFilters)
+			if value < minValue {
+				minValue = value
+			}
+			break
+
+		} else if s.lowerBound > f.upperBound || f.lowerBound > s.upperBound {
+			// outside (no fitler matches)
+			// check if we are on the last filter, if so move on to the next map as per instruction
+			if i == len(mapFilters[index].filters)-1 {
+				value := processSeedRange(s, index+1, mapFilters)
+				if value < minValue {
+					minValue = value
+				}
+				break
+			}
+			// if we don't fit the current filter, continue
+			continue
+		}
+
+	}
+	return minValue
+}
+
+func (mf *MapFilter) computeSeed(seed int) int {
 	value := seed
 	for _, f := range mf.filters {
 		if value >= f.lowerBound && value <= f.upperBound {
@@ -72,7 +184,7 @@ func (mf *MapFilter) compute(seed int) int {
 		}
 	}
 	if mf.nextMap != nil {
-		value = mf.nextMap.compute(value)
+		value = mf.nextMap.computeSeed(value)
 	}
 	return value
 }
@@ -113,7 +225,7 @@ type Filter struct {
 	boundDiff  int
 }
 
-func part2(f string) int {
+func part2_runs_in_about_3min(f string) int {
 	scanner := util.CreateScannerFromFile(f)
 	seeds := []int{}
 	var mapFilters []*MapFilter
@@ -148,7 +260,7 @@ func part2(f string) int {
 	var value int
 	for i := 0; i < len(seeds); i += 2 {
 		for j := seeds[i]; j <= seeds[i]+seeds[i+1]-1; j++ {
-			value = mapFilters[0].compute(j)
+			value = mapFilters[0].computeSeed(j)
 			if value < minValue {
 				minValue = value
 			}
